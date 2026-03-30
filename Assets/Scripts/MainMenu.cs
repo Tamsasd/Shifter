37ae1b8f5e36c58cc034a8e249f4e2dc7de9ac45
Zobi101 <bozsikdominik@gmail.com>
2026-03-30 17:47:33 +0200

    Implement main menu
    Settings is PoC as of now

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
