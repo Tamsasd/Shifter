c635ad85bb508d4c02dca0794824e5c2b9a5f20c
Horneczki Tamás <horneczki.tamas@mailbox.unideb.hu>
2026-03-31 21:45:25 +0200

    add: implemented outline effect on shiftable characters

--- Assets/Scripts/Characters/Shift.cs
+++ Assets/Scripts/Characters/Shift.cs
@@ -4,37 +4,90 @@ using UnityEngine;
 
 public class Shift : MonoBehaviour
 {
-    private HashSet<Character> shiftableCharacters = new HashSet<Character>();
+    private HashSet<Character> shiftableCharacters = new();
     private GameManager gameManager;
+    private List<Renderer> targetRenderers = new();
+    [SerializeField] private int outlineLayerIndex = 1;
 
     void Start()
     {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager = FindObjectOfType<GameManager>();
+
+        targetRenderers = new List<Renderer>(GetComponentsInChildren<Renderer>());
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && shiftableCharacters.Count > 0)
+        Character thisChar = GetComponent<Character>();
+
+        if (thisChar.HasControl() && Input.GetKeyDown(KeyCode.F) && shiftableCharacters.Count > 0)
         {
-            gameManager.setControlledObject(shiftableCharacters.First());
+            Character closest = shiftableCharacters
+                .Where(c => c != thisChar)
+                .OrderBy(c => Vector3.Distance(transform.position, c.transform.position))
+                .FirstOrDefault();
+
+            if (closest != null)
+            {
+                gameManager.setControlledObject(closest);
+                OnControlLost();
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Character thisCharacter = this.GetComponent<Character>();
+        Character thisCharacter = GetComponent<Character>();
+
         if (thisCharacter.HasControl() && other.CompareTag("Character"))
         {
-            shiftableCharacters.Add(other.GetComponent<Character>());
-
+            if (other.TryGetComponent(out Shift otherShift) && other.TryGetComponent(out Character otherChar))
+            {
+                shiftableCharacters.Add(otherChar);
+                otherShift.EnableOutline();
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Character"))
+        Character thisCharacter = GetComponent<Character>();
+
+        if (thisCharacter.HasControl() && other.CompareTag("Character"))
         {
-            shiftableCharacters.Remove(other.GetComponent<Character>());
+            if (other.TryGetComponent(out Shift otherShift) && other.TryGetComponent(out Character otherChar))
+            {
+                shiftableCharacters.Remove(otherChar);
+                otherShift.DisableOutline();
+            }
         }
+    }
+
+    public void EnableOutline()
+    {
+        foreach (Renderer renderer in targetRenderers)
+        {
+            renderer.renderingLayerMask |= (1u << outlineLayerIndex);
+        }
+    }
+
+    public void DisableOutline()
+    {
+        foreach (Renderer renderer in targetRenderers)
+        {
+            renderer.renderingLayerMask &= ~(1u << outlineLayerIndex);
+        }
+    }
+
+    public void OnControlLost()
+    {
+        foreach (var character in shiftableCharacters)
+        {
+            if (character.TryGetComponent(out Shift otherShift))
+            {
+                otherShift.DisableOutline();
+            }
+        }
+        shiftableCharacters.Clear();
     }
 }
