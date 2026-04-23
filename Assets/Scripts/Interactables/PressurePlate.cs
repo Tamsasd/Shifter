d919426b33ce5d81b4acd8629a4866f99fcf6647
Horneczki Tamás <horneczki.tamas@mailbox.unideb.hu>
2026-04-23 13:21:57 +0200

    add: added missing files to the repository

--- Assets/Scripts/Interactables/PressurePlate.cs
+++ Assets/Scripts/Interactables/PressurePlate.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PressurePlate : Interactable
 {
     [SerializeField] private float minimumMass = 1.0f;
+    [SerializeField] private float activationDelay = 2.0f; // Time in seconds before it activates
+
     private Animator animator;
     private float massOnButton = 0;
     private bool isActivated = false;
+    private float timeOnButton = 0f; // Tracks how long the mass has been sufficient
 
     // Start is called before the first frame update
     void Start()
@@ -18,41 +21,68 @@ public class PressurePlate : Interactable
     // Update is called once per frame
     void Update()
     {
+        // Check if there is enough mass on the plate
         if (massOnButton >= minimumMass)
         {
-            WhileActive();
+            if (!isActivated)
+            {
+                // Add the time passed since the last frame
+                timeOnButton += Time.deltaTime;
+
+                // Check if the delay has been reached
+                if (timeOnButton >= activationDelay)
+                {
+                    ActivatePlate();
+                }
+            }
+            else
+            {
+                // The plate is fully activated and the delay is over
+                WhileActive();
+            }
+        }
+        else
+        {
+            // Reset the timer if the mass drops below the threshold
+            timeOnButton = 0f;
+
+            // Deactivate the plate if it was previously active
+            if (isActivated)
+            {
+                DeactivatePlate();
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         massOnButton += GetMass(other);
-        Debug.Log("mass: " + massOnButton); 
-        if (!isActivated && massOnButton >= minimumMass)
-        { 
-            OnActivate();
-            animator.SetBool("buttonPressed", true);
-            isActivated = true;
-        }
+        Debug.Log("mass: " + massOnButton);
     }
 
     private void OnTriggerExit(Collider other)
     {
         massOnButton -= GetMass(other);
-        if (isActivated && massOnButton < minimumMass)
-        {
-            OnDeactivate();
-            animator.SetBool("buttonPressed", false);
-            isActivated = false;
-        }
     }
 
     private float GetMass(Collider other)
     {
-        Rigidbody otherRigidbody = other.attachedRigidbody;        
-        
+        Rigidbody otherRigidbody = other.attachedRigidbody;
+
         return otherRigidbody == null ? 0 : otherRigidbody.mass;
     }
 
+    private void ActivatePlate()
+    {
+        OnActivate();
+        animator.SetBool("buttonPressed", true);
+        isActivated = true;
+    }
 
+    private void DeactivatePlate()
+    {
+        OnDeactivate();
+        animator.SetBool("buttonPressed", false);
+        isActivated = false;
+    }
 }
